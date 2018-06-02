8df02812a57d2977f2d191639fcd0ebcc89b1eb3
Austin Vargason <vargasona@gmail.com>
2018-06-02 01:17:25 -0700

    first commit

---
+++ src/runWebToSpeech.bat
@@ -0,0 +1,1 @@
+powershell.exe -executionpolicy bypass -windowstyle hidden -file .\webToSpeech.ps1
---
+++ src/webToSpeech.ps1
@@ -0,0 +1,39 @@
+﻿<#
+Name: TestScript.ps1
+Description: Plays text to speech via powershell
+Author: Austin Vargason
+#>
+
+#text to speech function
+function start-TextToSpeech() {
+
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]$userString
+    )
+
+    #add-type speech
+    Add-Type -AssemblyName System.speech
+
+    $speachSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $speachSynth.Speak($userString)
+}
+
+function Get-WebText() {
+    param(
+        [Parameter(Mandatory=$true)]
+        [String]$url
+    )
+
+    $data = Invoke-WebRequest -Uri $url
+
+    $webText = $data.ParsedHTML.body.getElementsByTagName('p') | Select -ExpandProperty InnerText | Out-String
+
+    return $webText
+}
+
+#get input for url
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$url = [Microsoft.VisualBasic.Interaction]::InputBox("Enter a url", "URL", "https://en.wikipedia.org/wiki/Honda_Ridgeline")
+
+start-TextToSpeech -userString (Get-WebText -url $url)
